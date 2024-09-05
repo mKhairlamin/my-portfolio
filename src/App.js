4e4edaa3a9eead6d8d76787288830a9fcfd3705a
mKhairlamin <mkhairlamin@gmail.com>
2024-09-05 14:26:41 +0800

    Initial commit of portfolio

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+      </header>
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>Hi, I'm [Your Name], a web developer passionate about building web applications.</p>
+        </section>
+        <section>
+          <h2>Projects</h2>
+          <ul>
+            <li>Project 1: A cool project I worked on</li>
+            <li>Project 2: Another amazing project</li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
